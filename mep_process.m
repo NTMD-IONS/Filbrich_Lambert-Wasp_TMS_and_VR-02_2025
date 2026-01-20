bdb6d8b2b77f59b3fe2af19f674e61f85e4e32b8
cedlenoir <cedric.lenoir@uclouvain.be>
2026-01-20 18:12:10 +0100

    reorganize folders + add DC and linear detrend + add 2 loops of RMS check TODO : until no outliers + add second check on absolute threshold + plotting

--- mep_process.m
+++ mep_process.m
@@ -2,23 +2,29 @@
 % 
 % decide on folder and subject level naming of the acquisitions
 % sub-00$
-% 
+% main_folder contains the raw_data folder, pre-processed data folder,
+% results folder, (scripts folder?)
 % 
 % (sub-001 is test_giulia)
-% 
+% (sub-002 is Laurie)
+%
 % % Cédric Lenoir, NeTMeD, IoNS, UCLouvain, January 2026
 % 
 %% 1) LOAD DATA AND SET A FEW PARAMETERS
 
 % set the paths and initialize letswave
 % Select the general data folder which contains all subjects folders
-data_folder = uigetdir('C:\','Select data folder');
-results_folder = fullfile(data_folder,'results');
-if ~isfolder(results_folder)
-    mkdir(results_folder);
-else
-end
-addpath(genpath(data_folder))
+main_folder = uigetdir('C:\','Select main folder');
+cd(main_folder)
+raw_data_folder = fullfile(main_folder,'raw_data');
+results_folder = fullfile(main_folder,'results');
+pre_proc_folder = fullfile(main_folder,'pre-processed');
+
+if ~isfolder(raw_data_folder); mkdir(raw_data_folder); end
+if ~isfolder(results_folder); mkdir(results_folder); end
+if ~isfolder(pre_proc_folder); mkdir(pre_proc_folder); end
+
+addpath(genpath(main_folder))
 
 % initialize letswave 6
 % check if lw is already on the path if not select the folder of the toolobx
@@ -65,127 +71,182 @@ sub_info.TMS_triggers.pst = [ixd_tms_pst_start;ixd_tms_pst_stop];
 sub_info.comments = notes;
 
 % folder name of Visor EMG data
-subject_folder = fullfile(data_folder,strcat('sub-',subject_id),'Sessions');
-pre_proc_folder = fullfile(data_folder,'pre-processed',sprintf('sub-%s',subject_id));
-if ~isfolder(pre_proc_folder); mkdir(pre_proc_folder); end
+subject_folder = fullfile(raw_data_folder,strcat('sub-',subject_id),'Sessions');
+sub_pre_proc_folder = fullfile(pre_proc_folder,sprintf('sub-%s',subject_id));
+if ~isfolder(sub_pre_proc_folder); mkdir(sub_pre_proc_folder); end
 
 % list sessions
 session_list = dir(subject_folder);
-session_folder = session_list(~ismember({session_list.name}, {'.', '..'}));
+session_list = session_list(~ismember({session_list.name}, {'.', '..'}));
 
 % list EMG CNT files
-file_list = dir(fullfile(session_folder.folder,session_folder.name,'*emg.cnt*'));
+file_list = dir(fullfile(session_list.folder,session_list.name,'*emg.cnt*'));
 
 % import CNT file
-[out_data,~] = RLW_import_CNT(fullfile(session_folder.folder,session_folder.name,file_list.name));
+[out_data,~] = RLW_import_CNT(fullfile(session_list.folder,session_list.name,file_list.name));
 filename = file_list.name(1:end-8);
 savename = strcat('sub-',subject_id,'_',filename);
 out_data.header.name = savename;
-CLW_save(pre_proc_folder,out_data.header,out_data.data)
+CLW_save(sub_pre_proc_folder,out_data.header,out_data.data);
 clear out_data
-[header, data] = CLW_load(fullfile(pre_proc_folder,savename));
+[header, data] = CLW_load(fullfile(sub_pre_proc_folder,savename));
 sr = 1/header.xstep;
 
 % pre-processign steps in lw
-% DC removal
-
-
 % high pass filter Butterworth 4 Hz; order 4
 low_cutoff = 4;
 order = 4;
 [filt_header, filt_data] = RLW_butterworth_filter(header,data,'filter_type','highpass','low_cutoff',low_cutoff,'filter_order',order);
-filt_header.name = strcat([savename,' HPfilt']);
 
 % segmentation
 xstart = -0.2;
 xduration = 0.7;
 [seg_header, seg_data] = RLW_segmentation(filt_header, filt_data, {'1'},'x_start',xstart,'x_duration',xduration);
-seg_header.name = strcat([filt_header.name,' ep']);
 seg_header.chanlocs.labels = 'EMG1';
 
-% Linear detrend
+% DC & Linear detrend
+[dc_header, dc_data] = RLW_dc_removal(seg_header,seg_data,'linear_detrend',1);
+dc_header.name = strcat(header.name,' HPfilt ep DC');
 
+% save dataset
+CLW_save(sub_pre_proc_folder,dc_header, dc_data);
 
 % keep TMS events after rMT
-% build valid epochs vector
-valid_tms_idx = (ixd_tms_pre_start:ixd_tms_pre_stop);
-for iblock_pst = 1:4
-    valid_tms_idx = [valid_tms_idx ixd_tms_pst_start(iblock_pst):ixd_tms_pst_stop(iblock_pst)];
+% arrange epochs of valid TMS triggers for each TMS block
+valid_tms_idx = cell(5,1);
+for iblock = 1:5
+    if iblock == 1
+        valid_tms_idx{iblock,1} = (ixd_tms_pre_start:ixd_tms_pre_stop);
+    else
+        valid_tms_idx{iblock,1} = (ixd_tms_pst_start(iblock-1):ixd_tms_pst_stop(iblock-1));
+    end
 end
-seg_data = seg_data(valid_tms_idx,1,1,1,1,:);
-
-seg_header.datasize = size(seg_data);
-seg_header.events = seg_header.events(valid_tms_idx);
-% save lw dataset
-CLW_save(pre_proc_folder,seg_header, seg_data);
+for iblock = 1:5
+    [block_header{iblock,1}, block_data{iblock,1}] = RLW_arrange_epochs(dc_header, dc_data,valid_tms_idx{iblock,1});
+    block_header{iblock,1}.name = strcat(dc_header.name,' BLK ',num2str(iblock));
+    CLW_save(sub_pre_proc_folder,block_header{iblock,1}, block_data{iblock,1});
+end
 
 % define baseline window to check for baseline activity
 bsln_time_window = [-0.2 0];
 bsln_sample_window = [1 round(0.2*sr+1)];
 
-% check for baseline EMG activity
-mep_data = squeeze(seg_data);
+% First check: for baseline single trial EMG activity (RMS) per block
+% threshold is mean(RMS) + 2.5*std(RMS)
+% (as in Sulcova D. et al. bioRxiv 2022; Grandjean and Duque NIMG 2020)
+idx_exclud = cell(5,1);
+for iblock = 1:5
+    temp_data = squeeze(block_data{iblock,1});
 
-% compute RMS for each trial
-for itrial = 1:size(mep_data,1)
-    bsln_rms(itrial,1) = rms(mep_data(itrial,bsln_sample_window(1):bsln_sample_window(2)));
-end
-% threshold mean RMS +/- 3*SD (permissive)
-avg_bsln_rms = mean(bsln_rms,1);
-sd_bsln_rms = std(bsln_rms,1);
-thrshd = avg_bsln_rms+3*sd_bsln_rms;
-% loop until no outlier is found (exclude MEP + warning if baseline trial
-% RMS > mean RMS +/- 3*SD, until no outliers)
-idx = 1;
-idx_exclud = [];
-for itrial = 1:size(mep_data,1)
-    if bsln_rms(itrial,1) < avg_bsln_rms + (3*sd_bsln_rms)
-        temp_mep_data(idx,:) = mep_data(itrial,:);
-        idx = idx+1;
+    % compute RMS for each trial
+    for itrial = 1:size(temp_data,1)
+        bsln_rms{iblock,1}(itrial,1) = rms(temp_data(itrial,bsln_sample_window(1):bsln_sample_window(2)));
+    end
+    % threshold mean RMS +/- 3*SD (permissive)
+    avg_bsln_rms(iblock,1) = mean(bsln_rms{iblock,1});
+    sd_bsln_rms(iblock,1) = std(bsln_rms{iblock,1});
+    thrshld(iblock,1) = avg_bsln_rms(iblock,1)+2.5*sd_bsln_rms(iblock,1);
+    % loop until no outlier is found (exclude MEP + warning if baseline trial
+    % RMS > mean RMS +/- 2.5*SD, until no outliers)
+    idx_val = 1;
+    idx_exc = 1;
+    for itrial = 1:size(temp_data,1)
+        if bsln_rms{iblock,1}(itrial,1) < thrshld(iblock,1)
+            valid_data{iblock,1}(idx_val,:) = temp_data(itrial,:);
+            idx_val = idx_val+1;
+        else
+            idx_exclud{iblock,1}(idx_exc,1) = itrial;
+            idx_exc = idx_exc+1;
+        end
+    end
+    % if at least one MEP has been excluded redo the RMS check
+    if size(valid_data{iblock,1},1) < block_header{iblock,1}.datasize(1)
+
+        for itrial = 1:size(valid_data{iblock,1},1)
+            bsln_rms2{iblock,1}(itrial,1) = rms(valid_data{iblock,1}(itrial,bsln_sample_window(1):bsln_sample_window(2)));
+        end
+    % threshold mean RMS +/- 3*SD (permissive)
+    avg_bsln_rms2(iblock,1) = mean(bsln_rms2{iblock,1});
+    sd_bsln_rms2(iblock,1) = std(bsln_rms2{iblock,1});
+    thrshld2(iblock,1) = avg_bsln_rms2(iblock,1)+2.5*sd_bsln_rms2(iblock,1);
+    % loop until no outlier is found (exclude MEP + warning if baseline trial
+    % RMS > mean RMS +/- 3*SD, until no outliers)
+    idx_val = 1;
+    idx_exc = 1;
+    for itrial = 1:size(valid_data{iblock,1},1)
+        if bsln_rms2{iblock,1}(itrial,1) < thrshld2(iblock,1)
+            valid_data{iblock,1}(idx_val,:) = valid_data{iblock,1}(itrial,:);
+            idx_val = idx_val+1;
+        else
+            idx_exclud{iblock,1}(idx_exc,1) = itrial;
+            idx_exc = idx_exc+1;
+        end
+    end
     else
-        idx = idx;
-        idx_exclud = [idx_exclud itrial];
+    end
+    clear temp_data
+end
+
+% display the number of MEP excluded for each block
+clc
+for iblock = 1:5
+    if isempty(idx_exclud{iblock,1})
+    disp(strcat(['No MEPs exclude in block ',num2str(iblock)]))
+    else
+        disp(strcat([num2str(length(idx_exclud{iblock,1})),' MEPs excluded in block ',num2str(iblock)]))
     end
 end
 
+% Second check: threshold on baseline RMS exceeding +/-15 µV
+% (as in Sulcova et al. BioRxiv 2022; Morozova et al. Sci Reports 2024)
+abs_thrshld = 15;
+idx_exclud = cell(5,1);
+for iblock = 1:5
+    for itrial = 1:size(valid_data{iblock,1},1)
+        bsln_rms2{iblock,1}(itrial,1) = rms(valid_data{iblock,1}(itrial,bsln_sample_window(1):bsln_sample_window(2)));
+    end
 
-for itrial = 1:size(mep_data,1)
-    if isempty(idx_exclud)
-        disp(strcat([num2str(0),' MEPs excluded']))
-    else
-        disp(strcat([num2str(idx-1),' MEPs excluded']))
+    idx_val = 1;
+    idx_exc = 1;
+    for itrial = 1:size(valid_data{iblock,1},1)
+        if bsln_rms2{iblock,1}(itrial,1) < abs_thrshld
+            valid_data2{iblock,1}(idx_val,:) = valid_data{iblock,1}(itrial,:);
+            idx_val = idx_val+1;
+        else
+            idx_exclud{iblock,1}(idx_exc,1) = itrial;
+            idx_exc = idx_exc+1;
+        end
     end
 end
-% exclude MEP + warning if baseline noise max amplitude > 15 µV
-
 % plot MEP and criteria
 xval = -0.2:1/sr:0.5;
-for itrial = 1:size(mep_data,1)
-    f = figure('Color','w','Position',[500 500 1500 700]);
-    plot(xval,mep_data(itrial,:),'k','LineWidth',1)
-    xline(0,'--r','TMS','LabelOrientation','horizontal')
-    yline(0,'-k')
-    yline(bsln_rms(itrial,1),'--k','RMS','LabelOrientation','horizontal','LabelVerticalAlignment','top','LabelHorizontalAlignment','left')
-    yline(thrshd,'--g','threshold','LabelOrientation','horizontal','LabelVerticalAlignment','top')
-    ax = gca;
-    ax.TickDir = 'out';
-    ax.XLabel.String = 'time (s)';
-    ax.YLabel.String = 'amplitude (µV)';
-    ax.Box = 'off';
-    title(strcat(['sub-',subject_id,' MEP#',num2str(itrial)]))
-    pause()
-    
+for iblock = 1:5
+    for itrial = 1:size(idx_exclud{iblock,1},1)
+        f = figure('Color','w','Position',[0 0 1500 700]);
+        plot(xval,valid_data{iblock,1}(idx_exclud{iblock,1}(itrial,1),:),'k','LineWidth',1)
+        xline(0,'--r','TMS','LabelOrientation','horizontal')
+        yline(0,'-k')
+        yline(bsln_rms2{iblock,1}(idx_exclud{iblock,1}(itrial,1),1),'--k','RMS','LabelOrientation','horizontal','LabelVerticalAlignment','top','LabelHorizontalAlignment','left')
+        yline(thrshld(iblock,1),'--g','threshold','LabelOrientation','horizontal','LabelVerticalAlignment','top','LabelHorizontalAlignment','center')
+        yline(abs_thrshld,'--m','absolute threshold','LabelOrientation','horizontal','LabelVerticalAlignment','top','LabelHorizontalAlignment','right')
+        ax = gca;
+        ax.TickDir = 'out';
+        ax.XLabel.String = 'time (s)';
+        ax.YLabel.String = 'amplitude (µV)';
+        ax.YLim = [-200 200];
+        ax.Box = 'off';
+        title(strcat(['sub-',subject_id,' MEP#',num2str(idx_exclud{iblock,1}(itrial,1)),' in block ',num2str(iblock)]))
+        pause()
+    end
 end
 
-% define response time window for EMG, from trigger index to +200 ms
-% responseEMGWind = [trigger_idx trigger_idx+0.04*sr];
+% define MEP peak-to-peak amplitude criterion > 50 µV in specific window
+% extract MEP latencies
 
-% define MEP peak-to-peak amplitude criterion > 50 µV
 emg_threshold = 50;
 
 
 
-% squeeeze data and send it in workspace
 
 
 %% 2) PREPROCESS EMG DATA
